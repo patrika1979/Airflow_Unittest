5a2aa0a13163bd69f13bf12f750b8569ea19cefd
Pati <31842733+patrika1979@users.noreply.github.com>
2022-08-08 14:49:51 +0100

    Add files via upload

---
+++ code-pipeline_unittest_v2/commands-dev-pipeline.sh
@@ -0,0 +1,33 @@
+# Create the S3 bucket for the CodePipeline artifacts - This bucket must be globally unique so set your own
+aws s3 mb s3://cdt-dev-codepipeline-artifacts
+
+#create admin role to be assumed by Cloud9
+aws iam create-role --role-name Cloud9Admin --assume-role-policy-document file://cpAssumeRolePolicyDocumentAdmin
+aws iam put-role-policy --role-name Cloud9Admin --policy-name  cloud9-access --policy-document file://cpPolicyAdmin.json
+
+#create cloud9
+aws cloudformation deploy --stack-name cloud9 --template-file cloud9.yml --capabilities CAPABILITY_NAMED_IAM
+
+#attach role 
+aws iam attach-role-policy --role-name Cloud9Admin #--policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+
+# Create IAM roles and add iniline policies so that CodePipeline can interact with EKS through kubectl
+aws iam create-role --role-name AirflowCodePipelineServiceRole --assume-role-policy-document file://code-pipeline_unittest/cpAssumeRolePolicyDocument.json
+aws iam put-role-policy --role-name AirflowCodePipelineServiceRole --policy-name codepipeline-access --policy-document file://code-pipeline_unittest/cpPolicyDocument.json
+aws iam create-role --role-name AirflowCodeBuildServiceRole --assume-role-policy-document file://code-pipeline_unittest/cbAssumeRolePolicyDocument.json
+aws iam put-role-policy --role-name AirflowCodeBuildServiceRole --policy-name codebuild-access --policy-document file://code-pipeline_unittest/cbPolicyDocument.json
+
+# open the flux pipeline to set the bucket name you created under "ArtifactStore"
+vim airflow-materials-aws/code-pipeline/airflow-dev-pipeline.cfn.yml
+
+# Create the AWS CodePipeline using CloudFormation (This doesn't deploy the image as Flux handles it)
+#aws cloudformation create-stack --stack-name=airflow-dev-pipeline --template-body=file://airflow-materials-aws/section-6/code-pipeline/airflow-dev-pipeline.cfn.yml --parameters ParameterKey=GitHubUser,ParameterValue=marclamberti ParameterKey=GitHubToken,ParameterValue=cb53803446b0968e132e2e8ff729c7596fb0d7c8 ParameterKey=GitSourceRepo,ParameterValue=airflow-eks-docker ParameterKey=GitBranch,ParameterValue=dev
+
+#Run Cloduformation template with Parameters
+aws cloudformation create-stack --stack-name=airflow-dev-pipeline --template-body=file://airflow-dev-pipeline.cfn.yml --parameters=file://parameters.json --region=us-east-1
+
+#delete stack
+aws cloudformation delete-stack --stack-name airflow-dev-pipeline
+
+#update the pipeline
+aws cloudformation update-stack --stack-name=airflow-dev-pipeline --template-body=file://airflow-dev-pipeline.cfn.yml --parameters=file://parameters.json --region=us-east-1
